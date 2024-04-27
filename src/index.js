36f3c159822e4ed0a0e14828b67712173feb73a0
badmazzz <pmeshw12012004@gmail.com>
2024-04-27 15:48:16 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-    path: "./.env"
-})
+  path: "./.env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`The app is listening on ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("ERROR: ", err);
+  });
 
 /*
 (async () => {
