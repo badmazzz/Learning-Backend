88edefcef3e2e1d858fadd2738f36c699f6a394e
badmazzz <pmeshw12012004@gmail.com>
2024-05-03 22:36:43 +0530

    setup route for users

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
-
 app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
@@ -11,5 +10,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Import router
+
+import userRouter from "./routes/user.routes.js";
+
+//Route Declaration
+app.use("/api/v1/users", userRouter);
 
 export default app;
