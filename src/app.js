db704d7868e0b755537544d82f426ba163bf6725
badmazzz <pmeshw12012004@gmail.com>
2024-05-03 07:06:30 +0530

    add user and video models

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+const app = express();
+
 
 app.use(cors());
 
@@ -9,6 +11,5 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-const app = express();
 
 export default app;
